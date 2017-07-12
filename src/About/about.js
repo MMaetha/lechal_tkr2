1fd4672a93b99ea639c19e310c85eefb0abfea11
MMaetha <mod41744555@hotmail.com>
2017-07-12 16:06:20 +0900

    modified Contact Us page

--- src/About/about.js
+++ src/About/about.js
@@ -2,7 +2,6 @@ import React , { Component } from 'react';
 import 'bootstrap/dist/css/bootstrap.css';
 import { Button } from 'reactstrap';
 import { Container } from 'reactstrap'
-// import myImage from '../assets/shoes2.png'
 
 class about extends Component{
 render(){
